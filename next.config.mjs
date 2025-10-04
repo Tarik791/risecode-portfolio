125dabbba167e42d31f465d981a6971b077118f5
Tarik791 <terzotarik@gmail.com>
2025-10-04 16:02:44 +0200

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // GitHub Pages zahtijeva static export
   output: "export",
-
-  // Ako hostaš na https://USERNAME.github.io/REPO_NAME/
-  basePath: "/risecode-portfolio",
-  assetPrefix: "/risecode-portfolio/",
 };
 
 export default nextConfig;
